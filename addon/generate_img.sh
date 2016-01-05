3fb77a7156c29df4ffcd819654c6043cb25976fd
Jens Maus <mail@jens-maus.de>
2016-01-05 16:38:12 +0100

    added generate_img.sh script for generate a proper CCU addon package.

---
+++ addon/generate_img.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# script to generate the CCU addon package.
+
+# generate tempdir
+mkdir -p tmp
+rm -rf tmp/*
+
+# copy all relevant stuff
+cp -a update_script tmp/
+cp -a common tmp/
+cp -a rc.d tmp/
+cp -a www tmp/
+cp -a ccu1 tmp/
+cp -a ccu2 tmp/
+cp -a ccurm tmp/
+cp -a ../VERSION tmp/
+
+# copy hm_pdetect main script + config
+mkdir -p tmp/common/bin
+cp -a ../hm_pdetect.sh tmp/common/bin/
+cp -a ../hm_pdetect.conf.sample tmp/common/etc/hm_pdetect.conf
+
+# generate archive
+cd tmp
+tar --owner=root --group=root -czvf ../hm_pdetect-$(cat ../../VERSION).tar.gz *
+cd ..
+rm -rf tmp
