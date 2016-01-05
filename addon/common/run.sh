5478dde88b747cca2a10d5efdab05afe2584026a
Jens Maus <mail@jens-maus.de>
2016-01-05 16:45:08 +0100

    specify the conf file on the command-line in run.sh.

--- addon/common/run.sh
+++ addon/common/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export PATH=/usr/local/addons/hm_pdetect/bin:${PATH}
-/usr/local/addons/hm_pdetect/bin/hm_pdetect.sh >/var/log/hm_pdetect.log
+/usr/local/addons/hm_pdetect/bin/hm_pdetect.sh /usr/local/addons/hm_pdetect/etc/hm_pdetect.conf 2>&1 >/var/log/hm_pdetect.log
