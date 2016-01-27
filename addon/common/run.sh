2a68b295977c7d394f3b5202cda5283bc505efdc
Jens Maus <mail@jens-maus.de>
2016-01-27 21:55:51 +0100

    upon installing the addon an existing configuration will not be overwritten anymore.

--- addon/common/run.sh
+++ addon/common/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export PATH=/usr/local/addons/hm_pdetect/bin:${PATH}
 echo $(date) >/var/log/hm_pdetect.log
-echo >>/var/log/hm_pdetect.log
+echo "============================" >>/var/log/hm_pdetect.log
 /usr/local/addons/hm_pdetect/bin/hm_pdetect.sh /usr/local/addons/hm_pdetect/etc/hm_pdetect.conf 2>&1 >>/var/log/hm_pdetect.log
