14bac07dff0f45b281ba0aa3c9acc8ee437d5d2b
Jens Maus <mail@jens-maus.de>
2016-02-02 17:43:23 +0100

    applied changes to addon environment to match the new daemon mode recently implemented.

--- addon/common/run.sh
+++ addon/common/run.sh
@@ -1,5 +1,47 @@
 #!/bin/sh
-export PATH=/usr/local/addons/hm_pdetect/bin:${PATH}
-echo $(date) >/var/log/hm_pdetect.log
-echo "============================" >>/var/log/hm_pdetect.log
-/usr/local/addons/hm_pdetect/bin/hm_pdetect.sh /usr/local/addons/hm_pdetect/etc/hm_pdetect.conf 2>&1 >>/var/log/hm_pdetect.log
+#
+# wrapper script to execute hm_pdetect in non-daemon
+# mode with the possibility to run it a certain amount
+# of time by specifying the maximum iterations and
+# interval time as command-line options
+#
+# Example:
+# -------
+#
+# Runs hm_pdetect only once:
+#
+# $ /usr/local/addons/hm_pdetect/run.sh
+#
+# Runs hm_pdetect 10 times with a waittime of 5
+# seconds between each execution:
+#
+# $ /usr/local/addons/hm_pdetect/run.sh 10 5
+#
+# Copyright (c) 2016 Jens Maus <mail@jens-maus.de>
+#
+
+# directory path to hm_pdetect addon dir.
+ADDON_DIR=/usr/local/addons/hm_pdetect
+
+# set default settings (will be overwritten by config file)
+export HM_PROCESSLOG_FILE="/var/log/hm_pdetect.log"
+export CONFIG_FILE="${ADDON_DIR}/etc/hm_pdetect.conf"
+
+# the interval settings can be specified on the command-line
+if [ $# -gt 0 ]; then
+  export HM_INTERVAL_MAX=${1}
+  if [ $# -gt 1 ]; then
+    export HM_INTERVAL_TIME=${2}
+  else
+    export HM_INTERVAL_TIME=15
+  fi
+else
+  # otherwise do one iteration only with no
+  # defined interval time
+  export HM_INTERVAL_MAX=1
+  export HM_INTERVAL_TIME=
+fi
+
+# execute hm_pdetect in non-daemon mode
+export PATH="${ADDON_DIR}/bin:${PATH}"
+${ADDON_DIR}/bin/hm_pdetect.sh >/dev/null 2>&1
