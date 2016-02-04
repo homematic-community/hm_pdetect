dc7b34bfddc67717c5c90983bfd63ef12c362b1e
Jens Maus <mail@jens-maus.de>
2016-02-04 14:46:32 +0100

    adapted web configuration interface to latest feature changes.

--- addon/www/settings/inc/settings.tcl
+++ addon/www/settings/inc/settings.tcl
@@ -1,7 +1,7 @@
 set ADDONNAME "hm_pdetect"
 set FILENAME "/usr/local/addons/hm_pdetect/etc/hm_pdetect.conf"
 
-array set args { command INV HM_FRITZ_IP {} HM_FRITZ_USER {} HM_FRITZ_SECRET {} HM_CCU_PRESENCE_VAR {} HM_CCU_PRESENCE_VAR_LIST {} HM_CCU_PRESENCE_VAR_STR {} HM_CCU_PRESENCE_GUEST {} HM_CCU_PRESENCE_NOBODY {} HM_CCU_PRESENCE_PRESENT {} HM_CCU_PRESENCE_AWAY {} HM_USER_LIST {} HM_KNOWN_LIST_MODE {} HM_KNOWN_LIST {} }
+array set args { command INV HM_FRITZ_IP {} HM_FRITZ_USER {} HM_FRITZ_SECRET {} HM_CCU_PRESENCE_VAR {} HM_CCU_PRESENCE_LIST {} HM_CCU_PRESENCE_STR {} HM_CCU_PRESENCE_GUEST {} HM_CCU_PRESENCE_NOBODY {} HM_CCU_PRESENCE_USER {} HM_CCU_PRESENCE_PRESENT {} HM_CCU_PRESENCE_AWAY {} HM_USER_LIST {} HM_KNOWN_LIST_MODE {} HM_KNOWN_LIST {} HM_INTERVAL_TIME {} }
 
 proc utf8 {hex} {
     set hex [string map {% {}} $hex]
@@ -72,25 +72,36 @@ proc loadFile { fileName } {
 }
 
 proc loadConfigFile { } {
-    global FILENAME HM_FRITZ_IP HM_FRITZ_USER HM_FRITZ_SECRET HM_CCU_PRESENCE_VAR HM_CCU_PRESENCE_VAR_LIST HM_CCU_PRESENCE_VAR_STR HM_CCU_PRESENCE_GUEST HM_CCU_PRESENCE_NOBODY HM_CCU_PRESENCE_PRESENT HM_CCU_PRESENCE_AWAY HM_USER_LIST HM_KNOWN_LIST_MODE HM_KNOWN_LIST
+    global FILENAME HM_FRITZ_IP HM_FRITZ_USER HM_FRITZ_SECRET HM_CCU_PRESENCE_VAR HM_CCU_PRESENCE_LIST HM_CCU_PRESENCE_STR HM_CCU_PRESENCE_GUEST HM_CCU_PRESENCE_NOBODY HM_CCU_PRESENCE_USER HM_CCU_PRESENCE_PRESENT HM_CCU_PRESENCE_AWAY HM_USER_LIST HM_KNOWN_LIST_MODE HM_KNOWN_LIST HM_INTERVAL_TIME
     set conf ""
     catch {set conf [loadFile $FILENAME]}
 
     if { [string trim "$conf"] != "" } {
+        set HM_INTERVAL_MAX 0
+
         regexp -line {^HM_FRITZ_IP=\"(.*)\"$} $conf dummy HM_FRITZ_IP
         regexp -line {^HM_FRITZ_USER=\"(.*)\"$} $conf dummy HM_FRITZ_USER
         regexp -line {^HM_FRITZ_SECRET=\"(.*)\"$} $conf dummy HM_FRITZ_SECRET
         regexp -line {^HM_CCU_PRESENCE_VAR=\"(.*)\"$} $conf dummy HM_CCU_PRESENCE_VAR
-        regexp -line {^HM_CCU_PRESENCE_VAR_LIST=\"(.*)\"$} $conf dummy HM_CCU_PRESENCE_VAR_LIST
-        regexp -line {^HM_CCU_PRESENCE_VAR_STR=\"(.*)\"$} $conf dummy HM_CCU_PRESENCE_VAR_STR
+        regexp -line {^HM_CCU_PRESENCE_LIST=\"(.*)\"$} $conf dummy HM_CCU_PRESENCE_LIST
+        regexp -line {^HM_CCU_PRESENCE_STR=\"(.*)\"$} $conf dummy HM_CCU_PRESENCE_STR
         regexp -line {^HM_CCU_PRESENCE_GUEST=\"(.*)\"$} $conf dummy HM_CCU_PRESENCE_GUEST
         regexp -line {^HM_CCU_PRESENCE_NOBODY=\"(.*)\"$} $conf dummy HM_CCU_PRESENCE_NOBODY
+        regexp -line {^HM_CCU_PRESENCE_USER=\"(.*)\"$} $conf dummy HM_CCU_PRESENCE_USER
         regexp -line {^HM_CCU_PRESENCE_PRESENT=\"(.*)\"$} $conf dummy HM_CCU_PRESENCE_PRESENT
         regexp -line {^HM_CCU_PRESENCE_AWAY=\"(.*)\"$} $conf dummy HM_CCU_PRESENCE_AWAY
         regexp -line {^HM_USER_LIST=\((.*)\)$} $conf dummy HM_USER_LIST
         regexp -line {^HM_KNOWN_LIST_MODE=\"(.*)\"$} $conf dummy HM_KNOWN_LIST_MODE
         regexp -line {^HM_KNOWN_LIST=\"(.*)\"$} $conf dummy HM_KNOWN_LIST
+        regexp -line {^HM_INTERVAL_MAX=\"(.*)\"$} $conf dummy HM_INTERVAL_MAX
+        regexp -line {^HM_INTERVAL_TIME=\"(.*)\"$} $conf dummy HM_INTERVAL_TIME
 
+        # if HM_INTERVAL_MAX is 1 we have to uncheck the
+        # checkbox to signal that the interval stuff is disabled.
+        if { $HM_INTERVAL_MAX == 1 } {
+          set HM_INTERVAL_TIME 0
+        }
+ 
         # lets replace all spaces with newlines for the
         # textarea fields in the html code.
         regsub -all {\s+} $HM_KNOWN_LIST "\n" HM_KNOWN_LIST
@@ -112,15 +123,17 @@ proc saveConfigFile { } {
     set HM_FRITZ_USER [url-decode $args(HM_FRITZ_USER)]
     set HM_FRITZ_SECRET [url-decode $args(HM_FRITZ_SECRET)]
     set HM_CCU_PRESENCE_VAR [url-decode $args(HM_CCU_PRESENCE_VAR)]
-    set HM_CCU_PRESENCE_VAR_LIST [url-decode $args(HM_CCU_PRESENCE_VAR_LIST)]
-    set HM_CCU_PRESENCE_VAR_STR [url-decode $args(HM_CCU_PRESENCE_VAR_STR)]
+    set HM_CCU_PRESENCE_LIST [url-decode $args(HM_CCU_PRESENCE_LIST)]
+    set HM_CCU_PRESENCE_STR [url-decode $args(HM_CCU_PRESENCE_STR)]
     set HM_CCU_PRESENCE_GUEST [url-decode $args(HM_CCU_PRESENCE_GUEST)]
     set HM_CCU_PRESENCE_NOBODY [url-decode $args(HM_CCU_PRESENCE_NOBODY)]
+    set HM_CCU_PRESENCE_USER [url-decode $args(HM_CCU_PRESENCE_USER)]
     set HM_CCU_PRESENCE_PRESENT [url-decode $args(HM_CCU_PRESENCE_PRESENT)]
     set HM_CCU_PRESENCE_AWAY [url-decode $args(HM_CCU_PRESENCE_AWAY)]
     set HM_USER_LIST [url-decode $args(HM_USER_LIST)]
     set HM_KNOWN_LIST_MODE [url-decode $args(HM_KNOWN_LIST_MODE)]
     set HM_KNOWN_LIST [url-decode $args(HM_KNOWN_LIST)]
+    set HM_INTERVAL_TIME [url-decode $args(HM_INTERVAL_TIME)]
 
     # make sure to replace newline stuff and double whitespaces with single whitespaces
     # because in the config we don't allow newlines.
@@ -132,25 +145,42 @@ proc saveConfigFile { } {
     set HM_FRITZ_USER [str-escape $HM_FRITZ_USER]
     set HM_FRITZ_SECRET [str-escape $HM_FRITZ_SECRET]
     
-    # we set the IP of the CCU always to 127.0.0.1
+    # we set config options that should not be changeable on the CCU
     puts $fd "HM_CCU_IP=127.0.0.1"
+    puts $fd "HM_PROCESSLOG_FILE=\"/var/log/hm_pdetect.log\""
+    puts $fd "HM_DAEMON_PIDFILE=\"/var/run/hm_pdetect.pid\""
 
     # only add the following variables if they are NOT empty
     if { [string length $HM_FRITZ_IP] > 0 }              { puts $fd "HM_FRITZ_IP=\"$HM_FRITZ_IP\"" }
     if { [string length $HM_FRITZ_USER] > 0 }            { puts $fd "HM_FRITZ_USER=\"$HM_FRITZ_USER\"" }
     if { [string length $HM_FRITZ_SECRET] > 0 }          { puts $fd "HM_FRITZ_SECRET=\"$HM_FRITZ_SECRET\"" }
     if { [string length $HM_CCU_PRESENCE_VAR] > 0 }      { puts $fd "HM_CCU_PRESENCE_VAR=\"$HM_CCU_PRESENCE_VAR\"" }
-    if { [string length $HM_CCU_PRESENCE_VAR_LIST] > 0 } { puts $fd "HM_CCU_PRESENCE_VAR_LIST=\"$HM_CCU_PRESENCE_VAR_LIST\"" }
-    if { [string length $HM_CCU_PRESENCE_VAR_STR] > 0 }  { puts $fd "HM_CCU_PRESENCE_VAR_STR=\"$HM_CCU_PRESENCE_VAR_STR\"" }
+    if { [string length $HM_CCU_PRESENCE_LIST] > 0 }     { puts $fd "HM_CCU_PRESENCE_LIST=\"$HM_CCU_PRESENCE_LIST\"" }
+    if { [string length $HM_CCU_PRESENCE_STR] > 0 }      { puts $fd "HM_CCU_PRESENCE_STR=\"$HM_CCU_PRESENCE_STR\"" }
     if { [string length $HM_CCU_PRESENCE_GUEST] > 0 }    { puts $fd "HM_CCU_PRESENCE_GUEST=\"$HM_CCU_PRESENCE_GUEST\"" }
     if { [string length $HM_CCU_PRESENCE_NOBODY] > 0 }   { puts $fd "HM_CCU_PRESENCE_NOBODY=\"$HM_CCU_PRESENCE_NOBODY\"" }
+    if { [string length $HM_CCU_PRESENCE_USER] > 0 }     { puts $fd "HM_CCU_PRESENCE_USER=\"$HM_CCU_PRESENCE_USER\"" }
     if { [string length $HM_CCU_PRESENCE_PRESENT] > 0 }  { puts $fd "HM_CCU_PRESENCE_PRESENT=\"$HM_CCU_PRESENCE_PRESENT\"" }
     if { [string length $HM_CCU_PRESENCE_AWAY] > 0 }     { puts $fd "HM_CCU_PRESENCE_AWAY=\"$HM_CCU_PRESENCE_AWAY\"" }
     if { [string length $HM_KNOWN_LIST_MODE] > 0 }       { puts $fd "HM_KNOWN_LIST_MODE=\"$HM_KNOWN_LIST_MODE\"" }
+
+    if { $HM_INTERVAL_TIME == 0 } { 
+      puts $fd "HM_INTERVAL_MAX=\"1\""
+    } else {
+      puts $fd "HM_INTERVAL_TIME=\"$HM_INTERVAL_TIME\""
+    }
 
     # also add empty variables on purpose
     puts $fd "HM_USER_LIST=($HM_USER_LIST)"
     puts $fd "HM_KNOWN_LIST=\"$HM_KNOWN_LIST\""
     
     close $fd
+
+    # we have updated our configuration so lets
+    # stop/restart hm_pdetect
+    if { $HM_INTERVAL_TIME == 0 } { 
+      exec /usr/local/etc/config/rc.d/hm_pdetect stop &
+    } else {
+      exec /usr/local/etc/config/rc.d/hm_pdetect restart &
+    }
 }
--- addon/www/settings/inc/settings1.tcl
+++ addon/www/settings/inc/settings1.tcl
@@ -2,25 +2,34 @@ regsub -all {<%HM_FRITZ_IP%>} $content [string trim $HM_FRITZ_IP] content
 regsub -all {<%HM_FRITZ_USER%>} $content [string trim $HM_FRITZ_USER] content
 regsub -all {<%HM_FRITZ_SECRET%>} $content [string trim $HM_FRITZ_SECRET] content
 regsub -all {<%HM_CCU_PRESENCE_VAR%>} $content [string trim $HM_CCU_PRESENCE_VAR] content
-regsub -all {<%HM_CCU_PRESENCE_VAR_LIST%>} $content [string trim $HM_CCU_PRESENCE_VAR_LIST] content
-regsub -all {<%HM_CCU_PRESENCE_VAR_STR%>} $content [string trim $HM_CCU_PRESENCE_VAR_STR] content
+regsub -all {<%HM_CCU_PRESENCE_LIST%>} $content [string trim $HM_CCU_PRESENCE_LIST] content
+regsub -all {<%HM_CCU_PRESENCE_STR%>} $content [string trim $HM_CCU_PRESENCE_STR] content
 regsub -all {<%HM_CCU_PRESENCE_GUEST%>} $content [string trim $HM_CCU_PRESENCE_GUEST] content
 regsub -all {<%HM_CCU_PRESENCE_NOBODY%>} $content [string trim $HM_CCU_PRESENCE_NOBODY] content
+regsub -all {<%HM_CCU_PRESENCE_USER%>} $content [string trim $HM_CCU_PRESENCE_USER] content
 regsub -all {<%HM_CCU_PRESENCE_PRESENT%>} $content [string trim $HM_CCU_PRESENCE_PRESENT] content
 regsub -all {<%HM_CCU_PRESENCE_AWAY%>} $content [string trim $HM_CCU_PRESENCE_AWAY] content
 regsub -all {<%HM_USER_LIST%>} $content [string trim $HM_USER_LIST] content
 regsub -all {<%HM_KNOWN_LIST%>} $content [string trim $HM_KNOWN_LIST] content
+regsub -all {<%HM_INTERVAL_TIME%>} $content [string trim $HM_INTERVAL_TIME] content
 
 if {[string equal "all" $HM_KNOWN_LIST_MODE]} {
   set HM_KNOWN_LIST_MODE1 ""
   set HM_KNOWN_LIST_MODE2 "checked"
+  set HM_KNOWN_LIST_MODE3 ""
+} elseif {[string equal "off" $HM_KNOWN_LIST_MODE]} {
+  set HM_KNOWN_LIST_MODE1 ""
+  set HM_KNOWN_LIST_MODE2 ""
+  set HM_KNOWN_LIST_MODE3 "checked"
 } else {
   set HM_KNOWN_LIST_MODE1 "checked"
   set HM_KNOWN_LIST_MODE2 ""
+  set HM_KNOWN_LIST_MODE3 ""
 }
 
 regsub -all {<%HM_KNOWN_LIST_MODE1%>} $content [string trim $HM_KNOWN_LIST_MODE1] content
 regsub -all {<%HM_KNOWN_LIST_MODE2%>} $content [string trim $HM_KNOWN_LIST_MODE2] content
+regsub -all {<%HM_KNOWN_LIST_MODE3%>} $content [string trim $HM_KNOWN_LIST_MODE3] content
 
-puts "Content-type:text/html\n\n"
+puts "Content-Type: text/html; charset=utf-8\n\n"
 puts $content
